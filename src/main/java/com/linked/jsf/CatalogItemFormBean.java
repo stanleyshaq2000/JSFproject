3bc98bb06389b7a5df9096ad929e43b62c51fb5e
stanley <stanleyshaq2000@hotmail.com>
2021-03-17 18:46:54 -0500

    First JSF

---
+++ src/main/java/com/linked/jsf/CatalogItemFormBean.java
@@ -0,0 +1,42 @@
+package com.linked.jsf;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@RequestScoped
+@Named
+public class CatalogItemFormBean {
+
+	private String name;
+	
+	private String manufacturer;
+	
+	private String sku;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+	
+	
+}
